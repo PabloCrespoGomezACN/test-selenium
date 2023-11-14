f99b91d592558c5c3d2202c728afd8a821bb33be
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-14 16:27:07 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,14 @@
 print('Starting test ........')
 
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
