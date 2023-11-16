ee835aaa99298d50d45f18cd2935b39084a1cc93
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-16 13:52:59 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,11 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
+
+
 # Set options for not prompting DevTools information
-options = Options()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# ___________
+# options = Options()
+# options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 print("testing started")
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+
 
 # No need to provide executable_path, Selenium will search in the system's PATH
 driver = webdriver.Chrome(options=options)
