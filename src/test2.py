cbf2b144d95d2762e43e1dfd8e5cde169c6b93c6
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-29 15:24:16 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
+# Import the value of PARAM_APP_HOST_URL from hello.py
+from values import PARAM_APP_HOST_URL
+
 print('TESTING STARTED')
 
 options = Options()
@@ -13,7 +16,8 @@ options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=options)
 
 # driver.get("https://www.saucedemo.com/")
-driver.get('${PARAM_APP_HOST_URL}')
+#driver.get('${PARAM_APP_HOST_URL}')
+driver.get(PARAM_APP_HOST_URL)
 sleep(3)
 
 # Get page title
