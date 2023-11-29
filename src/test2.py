0fac42d448ed4dc00f5679ca78307e8fd1da6a31
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-29 11:15:05 +0100

    Create test2.py

---
+++ src/test2.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+print('TESTING STARTED')
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+
+# No need to provide executable_path, Selenium will search in the system's PATH
+driver = webdriver.Chrome(options=options)
+
+# driver.get("https://www.saucedemo.com/")
+driver.get('${PARAM_APP_HOST_URL}')
+sleep(3)
+
+# Get page title
+title = driver.title
+
+# Test if title is correct
+assert 'Swag Labs' in title
+print(f'TEST 0: {title} \nTest passed')
+
+# Close the driver
+driver.quit()
