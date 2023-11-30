973442b08d5fd94f7f0388584b2d550cddbf0a02
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-30 14:19:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,31 @@
-print('Repo feedback')
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 from time import sleep
 
+# Import the value of PARAM_APP_HOST_URL from hello.py
+from values import PARAM_APP_HOST_URL
 
-# Set options for not prompting DevTools information
+print('TESTING STARTED')
+
 options = Options()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
 
-print("testing started")
+# No need to provide executable_path, Selenium will search in the system's PATH
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.saucedemo.com/")
+
+# driver.get("https://www.saucedemo.com/")
+#driver.get('${PARAM_APP_HOST_URL}')
+driver.get(PARAM_APP_HOST_URL)
 sleep(3)
 
 # Get page title
 title = driver.title
 
 # Test if title is correct
-assert "Swag Labs" in title
-print("TEST 0: `title` test passed")
+assert 'Swag Labs' in title
+print(f'TEST 0: {title} \nTest passed')
 
 # Close the driver
 driver.quit()
