20fc65f2ef14b425a133af90a11d4c10373b2e17
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-30 11:24:11 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,6 +4,8 @@ from time import sleep
 
 # Import the value of PARAM_APP_HOST_URL from hello.py
 from values import PARAM_APP_HOST_URL
+from values import PARAM_PROJECT_NAMESPACE
+from values import PARAM_OPC_CLUSTER_NAME
 
 def run_test():
     print('TESTING STARTED')
@@ -18,7 +20,10 @@ def run_test():
 
     # driver.get("https://www.saucedemo.com/")
     # driver.get('${PARAM_APP_HOST_URL}')
-    driver.get(PARAM_APP_HOST_URL)
+
+    driver.get(f'https://app-{PARAM_PROJECT_NAMESPACE}.apps.{PARAM_OPC_CLUSTER_NAME}.acic-navan.com')
+    
+    # driver.get(PARAM_APP_HOST_URL)
     sleep(3)
 
     # Get page title
