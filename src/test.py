1292be4e540d00e5f089388f92dcc311da7fe864
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2023-11-30 10:22:36 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,27 +5,37 @@ from time import sleep
 # Import the value of PARAM_APP_HOST_URL from hello.py
 from values import PARAM_APP_HOST_URL
 
-print('TESTING STARTED')
+def run_test():
+    print('TESTING STARTED')
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
 
-# No need to provide executable_path, Selenium will search in the system's PATH
-driver = webdriver.Chrome(options=options)
+    # No need to provide executable_path, Selenium will search in the system's PATH
+    driver = webdriver.Chrome(options=options)
 
-# driver.get("https://www.saucedemo.com/")
-#driver.get('${PARAM_APP_HOST_URL}')
-driver.get(PARAM_APP_HOST_URL)
-sleep(3)
+    # driver.get("https://www.saucedemo.com/")
+    # driver.get('${PARAM_APP_HOST_URL}')
+    driver.get(PARAM_APP_HOST_URL)
+    sleep(3)
 
-# Get page title
-title = driver.title
+    # Get page title
+    title = driver.title
 
-# Test if title is correct
-assert 'Swag Labs' in title
-print(f'TEST 0: {title} \nTest passed')
+    # Test if title is correct
+    assert 'Swag Labs' in title
+    print(f'TEST 0: {title}\nTest passed')
 
-# Close the driver
-driver.quit()
+    # Close the driver
+    driver.quit()
+
+# Run the test and handle success or failure
+try:
+    run_test()
+    print("Testing was successful!")
+except AssertionError as e:
+    print(f"Testing failed: {str(e)}")
+except Exception as e:
+    print(f"An unexpected error occurred: {str(e)}")
