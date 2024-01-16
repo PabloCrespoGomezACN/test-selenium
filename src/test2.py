801a47626c6274ba6430515df785c013d1c2e6ff
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2024-01-16 15:44:43 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,19 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# Specify the URL of the Selenium Hub
-hub_url = "http://172.30.178.107:4444"
-
-# Specify the desired capabilities for the browser (e.g., Chrome)
-desired_capabilities = DesiredCapabilities.CHROME.copy()
-
-# Create a WebDriver instance pointing to the Selenium Hub
-driver = webdriver.Remote(command_executor=hub_url, options=webdriver.ChromeOptions())
-
-# Now, you can use 'driver' to interact with the Selenium Grid
-# For example:
+options = webdriver.Chrome()
+driver = webdriver.Remote(command_executor="http://localhost:4444", options=options)
 driver.get("https://www.saucedemo.com/")
 print(driver.title)
-
-# Close the WebDriver session
 driver.quit()
