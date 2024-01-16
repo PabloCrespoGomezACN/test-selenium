87aab893ce21c5473d09b138e9878561f34ac688
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2024-01-16 17:13:32 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,12 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 print('TESTING STARTED')
-
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
 
 # Specify the URL of the Selenium Grid Hub
 grid_url = "http://172.30.226.167:4444/wd/hub"
