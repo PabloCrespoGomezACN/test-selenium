fa2d88d6b343c3e13880e04125d816a2c88480d5
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2024-01-16 13:30:57 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,42 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from time import sleep
-from values import APP_URL
-from values import HOST_URL
 
-def run_test():
-    print('TESTING STARTED')
+grid_url = "https://selenium-hub-client-dev-pgoa-devops-namespace.apps.ocpairbgdev1.acic-navan.com/wd/hub"
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['platform'] = "LINUX"
 
-    capabilities = DesiredCapabilities.CHROME.copy()
-    capabilities['platform'] = 'LINUX'
+driver = webdriver.Remote(
+    command_executor=grid_url,
+    desired_capabilities=capabilities
+)
 
-    HOST_URL = "https://app-{{ devops_namespace }}.apps.{{ ocp_cluster_name }}.acic-navan.com"
-    # HOST_URL = "https://selenium-hub-client-dev-pgoa-devops-namespace.apps.ocpairbgdev1.acic-navan.com/ui"
+# Your test script here
 
-    driver = webdriver.Remote(
-        command_executor = HOST_URL
-        # command_executor = "https://selenium-hub-client-dev-pgoa-devops-namespace.apps.ocpairbgdev1.acic-navan.com/ui"
-    )
-    
-    driver.get(APP_URL)
-    
-    sleep(3)
-
-    # Get page title
-    title = driver.title
-
-    # Test if title is correct
-    assert 'Swag Labs' in title
-    print(f'TEST 0: {title}\nTest passed')
-
-    # Close the driver
-    driver.quit()
-
-# Run the test and handle success or failure
-try:
-    run_test()
-    print('Testing was successful!')
-# except AssertionError as e:
-#    print(f'Testing failed: {str(e)}')
-# except Exception as e:
-#    print(f'An unexpected error occurred: {str(e)}')
+driver.quit()
