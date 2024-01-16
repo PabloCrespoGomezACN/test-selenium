544c26e4b154f750b54d9e66b6586f5d00c8affd
PabloCrespoGomezACN <58037349+PabloCrespoGomezACN@users.noreply.github.com>
2024-01-16 13:47:22 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,15 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 
+# Replace with the URL of your Selenium Grid Hub
 grid_url = "https://selenium-hub-client-dev-pgoa-devops-namespace.apps.ocpairbgdev1.acic-navan.com/wd/hub"
-capabilities = DesiredCapabilities.CHROME.copy()
-capabilities['platform'] = "LINUX"
 
-driver = webdriver.Remote(
-    command_executor=grid_url,
-    desired_capabilities=capabilities
-)
+# Create a dictionary of desired capabilities
+desired_capabilities = {
+    "browserName": "chrome",
+    "version": "latest",
+    "platform": "ANY",
+    "name": "Your Test Name",
+}
 
-# Your test script here
+# Create a WebDriver instance pointing to the Grid
+driver = webdriver.Remote(command_executor=grid_url, desired_capabilities=desired_capabilities)
 
-driver.quit()
+try:
+    # Navigate to your web app
+    driver.get("https://www.saucedemo.com/")
+
+    # Get page title
+    title = driver.title
+
+    # Test if title is correct
+    assert 'Swag Labs' in title
+    print(f'TEST 0: {title}\nTest passed')
+
+finally:
+    # Close the browser window
+    driver.quit()
